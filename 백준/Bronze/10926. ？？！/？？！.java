d7b9bf8491184f0bd5a86f9d14c47f7f0d67689b
CommonFace <112067140+CommonFace@users.noreply.github.com>
2022-09-20 16:12:38 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        name = scanner.nextLine();
+        System.out.println(name + "??!");
+        
+    }
+}
