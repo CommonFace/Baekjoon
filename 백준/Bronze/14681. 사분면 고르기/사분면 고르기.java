92651a474035e4a9a9aabcdc71dfb3706b675771
CommonFace <112067140+CommonFace@users.noreply.github.com>
2022-09-25 16:37:01 +0900

    [Bronze V] Title: 사분면 고르기, Time: 204 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x, y;
+
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+
+        if (x < 0) {
+            if (y < 0) {
+                System.out.println(3);
+            } else if (y > 0) {
+                System.out.println(2);
+            }
+        } else if (x > 0) {
+            if (y < 0) {
+                System.out.println(4);
+            } else if (y > 0) {
+                System.out.println(1);
+            }
+        }
+    }
+}
