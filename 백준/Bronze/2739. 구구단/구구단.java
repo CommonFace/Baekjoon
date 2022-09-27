82be5149e29f8a483f4b4a0f79c2325f7b7c7ce3
CommonFace <112067140+CommonFace@users.noreply.github.com>
2022-09-27 16:03:30 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A;
+
+        A = scanner.nextInt();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(A + " * " + i + " = " + A*i);
+        }
+    }
+}
